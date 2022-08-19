033ed9facdd0f6b82906ae3fd78072d69abb4b85
ushchapovsky <ushchapovskysu@kursksmu.net>
2022-08-19 11:59:12 +0300

    123

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-{{ dd(Auth::user()->usergable) }}
+{{--12  dd(Auth::user()->usergable) 12--}}
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
